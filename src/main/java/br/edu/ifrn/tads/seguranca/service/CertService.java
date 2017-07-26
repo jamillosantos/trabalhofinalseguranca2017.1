3617de8848148141a73ea9752ae35438adf21b8c
J. Santos <jamillo@gmail.com>
2017-07-26 12:06:47 -0300

    Métodos para validação da assinatura de arquivos PARCIAL #5

--- src/main/java/br/edu/ifrn/tads/seguranca/service/CertService.java
+++ src/main/java/br/edu/ifrn/tads/seguranca/service/CertService.java
@@ -6,7 +6,17 @@ import sun.security.x509.X500Name;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.stream.Stream;
 
 @Named
 public class CertService
@@ -37,5 +47,124 @@ public class CertService
 		user.setPublicKey(publicKey);
 		this.userService.save(user);
 		return privateKey;
+	}
+
+	public PrivateKey privateKeyFromStream(InputStream stream)
+	{
+		try
+		{
+			stream.skip(28); // PEM header
+			byte[] fileContent = new byte[stream.available() - 25 /* PEM footer */];
+			stream.read(fileContent);
+			PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(fileContent));
+			KeyFactory fact = KeyFactory.getInstance("RSA");
+			PrivateKey priv = fact.generatePrivate(keySpec);
+			// It ensures the private key is not laying around the memory where is not needed
+			Arrays.fill(fileContent, (byte) 0);
+			return priv;
+		}
+		catch (Exception e)
+		{
+			return null;
+		}
+	}
+
+	public PublicKey publicKeyFromStream(InputStream stream)
+	{
+		try
+		{
+			byte[] fileContent = new byte[stream.available()];
+			stream.read(fileContent);
+			byte[] data = Base64.getDecoder().decode(fileContent);
+			X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
+			KeyFactory fact = KeyFactory.getInstance("RSA");
+			return fact.generatePublic(spec);
+		}
+		catch (Exception e)
+		{
+			return null;
+		}
+	}
+
+	public boolean publicKeyToStream(PublicKey key, OutputStream stream)
+	{
+		try
+		{
+			KeyFactory fact = KeyFactory.getInstance("RSA");
+			X509EncodedKeySpec spec = fact.getKeySpec(key,
+				X509EncodedKeySpec.class);
+			stream.write(Base64.getEncoder().encode(spec.getEncoded()));
+			return true;
+		}
+		catch (IOException | InvalidKeySpecException | NoSuchAlgorithmException e)
+		{
+			return false;
+		}
+	}
+
+	public boolean privateKeyToStream(PrivateKey key, OutputStream stream)
+	{
+		try
+		{
+			KeyFactory fact = KeyFactory.getInstance("RSA");
+			PKCS8EncodedKeySpec spec = fact.getKeySpec(key,
+				PKCS8EncodedKeySpec.class);
+			stream.write("-----BEGIN PRIVATE KEY-----\n".getBytes());
+			stream.write(Base64.getEncoder().encode(spec.getEncoded()));
+			stream.write("\n-----END PRIVATE KEY-----".getBytes());
+			return true;
+		}
+		catch (InvalidKeySpecException | IOException | NoSuchAlgorithmException e)
+		{
+			return false;
+		}
+	}
+
+	/*
+	public String generateSignature(User user, InputStream stream)
+	{
+		Signature signature = null;
+		try
+		{
+			signature = user.getPublicKey().getSig;
+
+			byte[] buffer = new byte[1024];
+			int len;
+			while ((len = stream.read(buffer)) >= 0)
+				signature.update(buffer, 0, len);
+			return Base64.getEncoder().encodeToString(signature.sign());
+		}
+		catch (SignatureException | NoSuchAlgorithmException | NoSuchProviderException | IOException e)
+		{
+			return null;
+		}
+	}
+	*/
+
+	public boolean checkSignature(User user, String signature, InputStream stream)
+	{
+		return this.checkSignature(user.getPublicKey(), signature, stream);
+	}
+
+	protected boolean checkSignature(PublicKey key, String signature, InputStream stream)
+	{
+		try
+		{
+			byte[] sigbytes = Base64.getDecoder().decode(signature);
+			Signature sig = Signature.getInstance("SHA256withRSA");
+			sig.initVerify(key);
+			byte[] buffer = new byte[1024];
+			int len;
+			while (stream.available() > 0)
+			{
+				len = stream.read(buffer);
+				sig.update(buffer, 0, len);
+			}
+			return sig.verify(sigbytes);
+		}
+		catch (IOException | SignatureException | InvalidKeyException | NoSuchAlgorithmException e)
+		{
+			return false;
+		}
 	}
 }
--- src/test/java/br/edu/ifrn/tads/seguranca/service/CertServiceTest.java
+++ src/test/java/br/edu/ifrn/tads/seguranca/service/CertServiceTest.java
@@ -1,18 +1,18 @@
 package br.edu.ifrn.tads.seguranca.service;
 
 import br.edu.ifrn.tads.seguranca.TrabalhoFinalApplication;
-import br.edu.ifrn.tads.seguranca.model.Group;
-import br.edu.ifrn.tads.seguranca.model.Permission;
 import br.edu.ifrn.tads.seguranca.model.User;
-import org.hibernate.validator.internal.util.IgnoreJava6Requirement;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 
 import static org.testng.Assert.*;
 
@@ -32,7 +32,41 @@ public class CertServiceTest extends AbstractTestNGSpringContextTests
 	{
 		User user = new User("GI Joe", "john@doe.com", "12345");
 		this._userService.save(user);
-		PrivateKey key = this._service.generate(user);
-		assertNotNull(user.getPublicKey());
+		PrivateKey privateKey = this._service.generate(user);
+		PublicKey publicKey = user.getPublicKey();
+		assertNotNull(publicKey);
+		assertNotNull(privateKey);
+
+		//
+		// this._service.publicKeyToStream(publicKey, new FileOutputStream("/home/jsantos/public.key"));
+		// this._service.privateKeyToStream(privateKey, new FileOutputStream("/home/jsantos/private.pem"));
+	}
+
+	@Test
+	public void testCheckSignature()
+	{
+		PublicKey publicKey = this._service.publicKeyFromStream(this.getClass().getClassLoader().getResourceAsStream("public.key"));
+		InputStream fs = this.getClass().getClassLoader().getResourceAsStream("fileToHash");
+		assertTrue(this._service.checkSignature(
+			publicKey,
+			"NXO7dix7CUGmBkzpkkH3lC33Ch0FUy3v887cv1Fp+YGZzARwIEanHn3+x5LU7i0t1exqGk5dWj9+" +
+				"GlJwNnpTocfTP0PVsv1sm6SL7Hsfh0hjRsQAcMa2JpRdY1AhYSD1KPi7lgLwMmn7UoQ4klJVdSDZ" +
+				"9ybgKw9Btg5JNMV+kCM=",
+			fs
+		));
+	}
+
+	@Test
+	public void testCheckSignatureFail()
+	{
+		PublicKey publicKey = this._service.publicKeyFromStream(this.getClass().getClassLoader().getResourceAsStream("public.key"));
+		InputStream fs = this.getClass().getClassLoader().getResourceAsStream("fileToHash2");
+		assertFalse(this._service.checkSignature(
+			publicKey,
+			"NXO7dix7CUGmBkzpkkH3lC33Ch0FUy3v887cv1Fp+YGZzARwIEanHn3+x5LU7i0t1exqGk5dWj9+" +
+				"GlJwNnpTocfTP0PVsv1sm6SL7Hsfh0hjRsQAcMa2JpRdY1AhYSD1KPi7lgLwMmn7UoQ4klJVdSDZ" +
+				"9ybgKw9Btg5JNMV+kCM=",
+			fs
+		));
 	}
 }
